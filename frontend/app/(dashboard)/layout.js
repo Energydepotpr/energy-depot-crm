7beffdbdaab24f3dc57ee82574bc56f3373c81a0
Energydepotpr <franciscostuar90@gmail.com>
2026-05-18 17:50:29 -0400

    Sidebar: Calendario al menú principal (no oculto en 'Más')

--- frontend/app/(dashboard)/layout.js
+++ frontend/app/(dashboard)/layout.js
@@ -48,6 +48,7 @@ const NAV_SIDEBAR_MAIN = [
   { href: '/leads',        label: 'Leads',      tKey: 'nav.leads',     icon: 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z' },
   { href: '/cotizaciones', label: 'Cotizar',    tKey: 'nav.cotizar',   icon: 'M9 7H6a2 2 0 00-2 2v9a2 2 0 002 2h9a2 2 0 002-2v-3M9 7V5a2 2 0 012-2h2a2 2 0 012 2v2M9 7h6m-6 4h3m6-1l-3 3-1.5-1.5' },
   { href: '/inbox',        label: 'Chats',      tKey: 'nav.chats',     icon: 'M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z', badge: true },
+  { href: '/citas',        label: 'Calendario', tKey: 'nav.citas',     icon: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z' },
   { href: '/email',        label: 'Correo',     tKey: 'nav.email',     icon: 'M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z' },
   { href: '/alerts',       label: 'Alertas',    tKey: 'nav.alerts',    icon: 'M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9', alertBadge: true },
 ];
@@ -70,7 +71,6 @@ const NAV_SIDEBAR_EXTRA = [
   */
   // Marketing
   { href: '/marketing',    label: 'Marketing ROI', tKey: 'nav.marketing',   icon: 'M3 3v18h18M7 14l3-3 4 4 5-7', section: 'Marketing', sKey: 'sec.marketing' },
-  { href: '/citas',        label: 'Calendario',    tKey: 'nav.citas',       icon: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z', section: 'Comunicación', sKey: 'sec.comms' },
   // Comunicación
   { href: '/contacts',     label: 'Contactos',    tKey: 'nav.contacts',      icon: 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z', section: 'Comunicación', sKey: 'sec.comms' },
   { href: '/equipo',       label: 'Equipo',       tKey: 'nav.team',          icon: 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z', section: 'Comunicación', sKey: 'sec.comms' },
